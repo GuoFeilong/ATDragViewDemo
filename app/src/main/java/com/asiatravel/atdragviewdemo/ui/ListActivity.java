23e0427fe95748e2585ba088b0459978ec6d3b0d
GuoFeilong <guofeilongmr@qq.com>
2016-11-21 21:46:49 +0800

    测试
    测试

--- app/src/main/java/com/asiatravel/atdragviewdemo/ui/ListActivity.java
+++ app/src/main/java/com/asiatravel/atdragviewdemo/ui/ListActivity.java
@@ -38,6 +38,10 @@ public class ListActivity extends AppCompatActivity {
         });
     }
 
+    private String testALV(String data){
+        return "";
+    }
+
     class ListAdapter extends BaseAdapter {
 
         @Override
